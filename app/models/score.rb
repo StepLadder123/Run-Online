0b98d8c49eb02f24c211fa46081ee3b6c22d11ed
StepLadder123 <makito.takatori@gmail.com>
2021-08-24 23:03:22 +0900

    scoreモデルの編集
    - バリデーションの記述
    - アソシエーションの記述

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,2 +1,26 @@
 class Score < ApplicationRecord
+  belongs_to :user
+  # belongs_to :challenge
+  # has_one :course
+  # has_many :comments
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+
+  with_options presence: true do
+    validates :image
+    validates :date
+    validates :hour
+    validates :minute
+    validates :second
+  end
+  
+  with_options presence: true, numericality: { greater_than: 0} do
+    validates :distance
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :area_id
+  end
 end
