2bc3b791603611e1dab5c328502c7ae9ee4a5aae
StepLadder123 <makito.takatori@gmail.com>
2021-08-24 11:02:08 +0900

    マイグレーションファイルの編集
    - scoreテーブルのカラム編集

--- db/migrate/20210824003728_create_scores.rb
+++ db/migrate/20210824003728_create_scores.rb
@@ -1,7 +1,13 @@
 class CreateScores < ActiveRecord::Migration[6.0]
   def change
     create_table :scores do |t|
-
+      t.integer :distance,    null: false
+      t.time :time,           null: false
+      t.integer :area_id,     null: false
+      t.date :date,           null: false
+      t.boolean :private
+      t.integer :challenge_id
+      t.references :user,     null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_081148) do
+ActiveRecord::Schema.define(version: 2021_08_24_003728) do
+
+  create_table "scores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "distance", null: false
+    t.time "time", null: false
+    t.integer "area_id", null: false
+    t.date "date", null: false
+    t.boolean "private"
+    t.integer "challenge_id"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_scores_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_08_23_081148) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "scores", "users"
 end
