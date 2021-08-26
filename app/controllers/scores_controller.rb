6b4cb523530937e277db65d55d658624613fb00b
StepLadder123 <makito.takatori@gmail.com>
2021-08-26 12:03:28 +0900

    scoresコントローラーの編集
    - destroyアクションの追加
    - move_to_signed_inメソッドにdestroyを追加
    - set_scoreメソッドに集約

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,5 +1,6 @@
 class ScoresController < ApplicationController
   before_action :move_to_signed_in, except: [:index]
+  before_action :set_score, only: [:show, :destroy]
 
   def index
     @scores = Score.includes(:user).order("created_at DESC")
@@ -19,15 +20,23 @@ class ScoresController < ApplicationController
   end
 
   def show
-    @score = Score.find(params[:id])
   end
-
+  
+  def destroy
+    @score.destroy
+    redirect_to root_path
+  end
+  
   private
-
+  
   def move_to_signed_in
     unless user_signed_in?
       redirect_to '/users/sign_in'
     end
+  end
+  
+  def set_score
+    @score = Score.find(params[:id])
   end
 
   def score_params
