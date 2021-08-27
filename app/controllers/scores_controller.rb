4e004c027a634840eada1981210798a05fb3286e
StepLadder123 <makito.takatori@gmail.com>
2021-08-27 15:57:31 +0900

    scoresコントローラーの編集
    - listアクションの定義
    - privateメソッド（sort_action, sort_column）を定義し、ヘルパーメソッドに連携

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,15 +1,16 @@
 class ScoresController < ApplicationController
   before_action :move_to_signed_in, except: [:index]
   before_action :set_score, only: [:show, :destroy]
+  helper_method :sort_column, :sort_direction
 
   def index
     @scores = Score.includes(:user).order("created_at DESC")
   end
-
+  
   def new
     @score = Score.new
   end
-
+  
   def create
     @score = Score.new(score_params)
     if @score.save
@@ -18,7 +19,7 @@ class ScoresController < ApplicationController
       render :new
     end
   end
-
+  
   def show
     @comment = Comment.new
     @comments = @score.comments.includes(:user)
@@ -29,6 +30,10 @@ class ScoresController < ApplicationController
     redirect_to root_path
   end
   
+  def list
+    @scores = Score.order("#{sort_column} #{sort_direction}")
+  end
+
   private
   
   def move_to_signed_in
@@ -44,4 +49,12 @@ class ScoresController < ApplicationController
   def score_params
     params.require(:score).permit(:image, :distance, :hour, :minute, :second, :area_id, :date, :private, :challenge_id).merge(user_id: current_user.id)
   end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'desc'
+   end
+   
+   def sort_column
+    Score.column_names.include?(params[:sort]) ? params[:sort] : 'distance'
+   end
 end
