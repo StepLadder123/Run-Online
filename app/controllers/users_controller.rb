76300255bc66651d5202cbdd74505e0cf692b2f6
StepLadder123 <makito.takatori@gmail.com>
2021-09-03 18:29:11 +0900

    コントローラーの編集
    - usersコントローラーのedit, updateアクションの生成
    - authenticate_user!メソッドの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,39 @@
 class UsersController < ApplicationController
-  before_action :move_to_signed_in
+  before_action :authenticate_user!
+  before_action :set_user
 
   def show
-    @user = User.find_by(id: params[:id])
     @scores = Score.all.where(user_id: params[:id]).order(date: :desc, id: :desc)
     user_score
   end
-
-  private
-
-  def move_to_signed_in
-    unless user_signed_in?
-      redirect_to '/users/sign_in'
+  
+  def edit
+    unless @user == current_user
+      redirect_to user_path(@user)
     end
+  end
+  
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def set_user
+    @user = User.find_by(id: params[:id])
   end
 
   def user_score
     @term_year = Time.now.all_year
     @term_month = Time.now.all_month
     @term_week = Time.now.all_week
+  end
+
+  def user_params
+    params.fetch(:user, {}).permit(:username)
   end
 end
